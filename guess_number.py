a9bc92f51868c383e146d7872606742e3d550ddd
Jeralyn Saenz <saenz.388j@gmail.com>
2024-02-06 23:19:07 -0600

    Modified code to limit the guesses made

--- guess_number.py
+++ guess_number.py
@@ -2,20 +2,33 @@
 number = 10
 print("I'm thinking of a number...")
 
+count = 5
 while True:
     guess = input("What number am I thinking of? ")
 
-    if int(guess) == number:
-        print("Congratulations! You guessed the right number.")
-        break
     if guess == 'q':
         print(f'The number was {number}')    
         break
+    if int(guess) == number:
+        print("Congratulations! You guessed the right number.")
+        break
     if guess != number:
-         print(f'Incorrect guess. Try again.')
+        if int(guess) > number:
+            print('Too high! Try again. ')
+        if int(guess) < number:
+            print('Too low! Try again ')
+        count -= 1
+        print(f'You have {count} guesses left.')
+        if count == 0:
+            print('You ran out of guesses')
+            break
 
 
 
 
 
 
+
+
+    if count == 0:
+        break
